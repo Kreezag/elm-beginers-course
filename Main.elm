7bad556007d0a8d1ea694019348c8799a5602374
Kuchuk Andrey <kreezag@gmail.com>
2019-08-20 23:01:58 +0300

    functions_1

--- Main.elm
+++ Main.elm
@@ -3,6 +3,22 @@ module Main exposing (main)
 import Html exposing (..)
 
 
+add a b =
+    a ++ b
+
+
+addNum a b =
+    a + b
+
+
+testResult =
+    addNum 1 2 |> addNum 3
+
+
+result =
+    add "Hello " "world!"
+
+
 main : Html msg
 main =
-    text "Hello world!"
+    text (String.fromInt testResult)
