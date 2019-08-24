a79f984bac2141f25c391bcb8ec9b138ce259344
Kuchuk Andrey <kreezag@gmail.com>
2019-08-25 00:07:45 +0300

    add type-annotation exercise

---
+++ type-anotation/Exercises.elm
@@ -0,0 +1,46 @@
+module Main exposing (main)
+
+import Bool.Extra exposing (..)
+import Html exposing (..)
+import Json.Decode exposing (..)
+import Json.Encode exposing (..)
+
+
+type alias Card =
+    { name : String, qty : Int, freeQty : Int }
+
+
+card : List Card
+card =
+    [ { name = "Lemon", qty = 1, freeQty = 0 }
+    , { name = "Apple", qty = 5, freeQty = 0 }
+    , { name = "Pear", qty = 10, freeQty = 0 }
+    ]
+
+
+makeFreeQty : Int -> Int -> Card -> Card
+makeFreeQty sampleQty sampleFreeQty item =
+    if item.qty >= sampleQty then
+        { item
+            | freeQty = sampleFreeQty
+        }
+
+    else
+        item
+
+
+newCard : List Card
+newCard =
+    List.map (makeFreeQty 5 1 >> makeFreeQty 10 3) card
+
+
+main : Html msg
+main =
+    Html.div []
+        [ Html.div []
+            [ text (Debug.toString newCard)
+            ]
+        , Html.div []
+            [ text (Debug.toString card)
+            ]
+        ]
